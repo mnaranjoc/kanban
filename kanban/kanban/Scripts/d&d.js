9c11f59d92a998c0a01a58413b15c1e0d5df2749
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-22 23:58:27 -0500

    d&d continue

--- kanban/kanban/Scripts/d&d.js
+++ kanban/kanban/Scripts/d&d.js
@@ -1,35 +1,36 @@
-﻿var todo = document.querySelector('.todo');
+﻿var dragelements = document.querySelectorAll('.dragelement');
+for (var dragelement of dragelements) {
+    dragelement.addEventListener('dragstart', dragStart);
+    dragelement.addEventListener('dragend', dragEnd);
+}
 
-var tds = document.querySelectorAll('.todo');
+var dropelements = document.querySelectorAll('.dropelement');
+for (var dropelement of dropelements) {
+    dropelement.addEventListener('dragover', dragOver);
+    dropelement.addEventListener('dragenter', dragEnter);
+    dropelement.addEventListener('dragleave', dragLeave);
+    dropelement.addEventListener('drop', dragDrop);
+}
 
-todo.addEventListener('dragstart', dragStart);
-todo.addEventListener('dragend', dragEnd);
-todo.addEventListener('dragover', dragOver);
-todo.addEventListener('dragenter', dragEnter);
-todo.addEventListener('dragleave', dragLeave);
-todo.addEventListener('drop', dragDrop);
-
-// Drag Functions
+// Drag functions
 function dragStart() {
     console.log('dragStart');
 }
-
 function dragEnd() {
     console.log('dragEnd');
 }
-
-function dragOver() {
+// Drop functions
+function dragOver(e) {
+    e.preventDefault();
     console.log('dragOver');
 }
-
-function dragEnter() {
+function dragEnter(e) {
+    e.preventDefault();
     console.log('dragEnter');
 }
-
 function dragLeave() {
     console.log('dragLeave');
 }
-
 function dragDrop() {
     console.log('dragDrop');
 }
