e343b7deccba5514801e1c31d4e585d60b56e3a8
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-22 23:19:45 -0500

    d&d testing

---
+++ kanban/kanban/Scripts/d&d.js
@@ -0,0 +1,35 @@
+﻿var todo = document.querySelector('.todo');
+
+var tds = document.querySelectorAll('.todo');
+
+todo.addEventListener('dragstart', dragStart);
+todo.addEventListener('dragend', dragEnd);
+todo.addEventListener('dragover', dragOver);
+todo.addEventListener('dragenter', dragEnter);
+todo.addEventListener('dragleave', dragLeave);
+todo.addEventListener('drop', dragDrop);
+
+// Drag Functions
+function dragStart() {
+    console.log('dragStart');
+}
+
+function dragEnd() {
+    console.log('dragEnd');
+}
+
+function dragOver() {
+    console.log('dragOver');
+}
+
+function dragEnter() {
+    console.log('dragEnter');
+}
+
+function dragLeave() {
+    console.log('dragLeave');
+}
+
+function dragDrop() {
+    console.log('dragDrop');
+}
